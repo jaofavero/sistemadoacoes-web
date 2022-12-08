47519898e69b4f0ea5a9ccfc67f35eecd8e61e79
LennerPereira <lenner.pereira@estudante.iftm.edu.br>
2022-12-08 09:30:23 -0300

    Adicionando arquivos estáticos

---
+++ target/classes/static/js/script.js
@@ -0,0 +1,21 @@
+//Validação formulário de login
+function submeterFormLogin()
+{
+    let login = document.querySelector("#login");
+    let senha = document.querySelector("#senha");
+
+    if(login.value == "")
+    {
+        alert("Login está vazio, por favor digite um login antes de entrar no programa.");
+        login.focus();
+        return false;
+    }
+    if(senha.value == "")
+    {
+        alert("Senha está vazia, por favor digite uma senha antes de entrar no programa.");
+        login.focus();
+        return false;
+    }
+    return true;
+}
+ 
