2c9f3dc04d0895967515934933710cdcc7ab28f0
jivoc <4422822-PatoAssado@users.noreply.gitlab.com>
2022-12-19 13:18:42 -0300

    add ProdutosRepository

--- src/main/java/web/sistemaDoacoes/service/ProdutosService.java
+++ src/main/java/web/sistemaDoacoes/service/ProdutosService.java
@@ -1,7 +1,32 @@
 package web.sistemaDoacoes.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import web.sistemaDoacoes.model.Produtos;
+import web.sistemaDoacoes.repository.ProdutosRepository;
+
+
+@Service
 public class ProdutosService {
 
+	@Autowired
+	private ProdutosRepository produtosRepository;
 	
+	@Transactional
+	public void salvar(Produtos produtos) {
+		produtosRepository.save(produtos);
+	}
+	
+	@Transactional
+	public void alterar(Produtos produtos) {
+		produtosRepository.save(produtos);
+	}
+	
+	@Transactional
+	public void remover(Long codigo) {
+		produtosRepository.deleteById(codigo);
+	}
 	
 }
