d98356fe33ca77f91ff194d377f31ae5baedbbcc
jivoc <4422822-PatoAssado@users.noreply.gitlab.com>
2022-12-20 02:55:06 -0300

    Correct usuario error

--- src/main/java/web/sistemaDoacoes/model/Usuario.java
+++ src/main/java/web/sistemaDoacoes/model/Usuario.java
@@ -1,26 +1,43 @@
 package web.sistemaDoacoes.model;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
+import web.sistemaDoacoes.model.Usuario;
+import web.sistemaDoacoes.model.Papel;
+
 @Entity
 @Table (name = "usuario")
-public class Usuario {
+public class Usuario implements Serializable {
 
-	
+	private static final long serialVersionUID = 5757384541654785800L;
 	@Id
 	@SequenceGenerator(name = "gerador3", sequenceName = "usuario_codigo_seq", allocationSize = 1)
 	@GeneratedValue(generator = "gerador3", strategy = GenerationType.SEQUENCE)
 	private Long id;
 	private String login;
 	private String senha;
-	private int cargo;	
+	@Column(name = "nome_usuario")
+	private String nomeUsuario;
+	private boolean ativo;
+	@ManyToMany
+	@JoinTable(name = "usuario_papel", joinColumns = @JoinColumn(name = "codigo_usuario"), inverseJoinColumns = @JoinColumn(name = "codigo_papel"))
+	private List<Papel> papeis = new ArrayList<>();
+	
+	
 	
 	public Long getId() {
 		return id;
@@ -40,22 +57,17 @@ public class Usuario {
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
-	public int getCargo() {
-		return cargo;
-	}
-	public void setCargo(int cargo) {
-		this.cargo = cargo;
-	}
+	
 	
 	
 	@Override
 	public String toString() {
-		return "id: " + id + "\nlogin:" + login + "\nsenha=" + senha + "\ncargo=" + cargo;
+		return "id: " + id + "\nlogin:" + login + "\nsenha=" + senha;
 	}
 	
 	@Override
 	public int hashCode() {
-		return Objects.hash(cargo, id, login, senha);
+		return Objects.hash( id, login, senha);
 	}
 	@Override
 	public boolean equals(Object obj) {
@@ -66,10 +78,13 @@ public class Usuario {
 		if (getClass() != obj.getClass())
 			return false;
 		Usuario other = (Usuario) obj;
-		return cargo == other.cargo && Objects.equals(id, other.id) && Objects.equals(login, other.login)
-				&& Objects.equals(senha, other.senha);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
-
 	
 	
 	
